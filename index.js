0819bc42aeb843b3c27482d76c3e6a91e0fc6002
Hugoren Martinako <aumpfbahn@gmail.com>
2020-12-13 12:59:08 +0100

    refactor files & functions

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ import { transition } from "d3-transition"
 import { scaleQuantile } from "d3-scale"
 import { schemeGreens } from "d3-scale-chromatic"
 import { feature } from "topojson-client"
-import data from "./data/valladolid.topo.json"
+import TOPOJSON_PATH from "url:./valladolid.topo.json"
+import REPORT_PATH from "url:./report.csv"
 
 // constants
 const INTERVAL_TIME = 1000
 const DELIMITER = ";"
-const REPORT_PATH = "./data/report.csv"
+const projection = geoMercator()
+const TOPOJSON_PROPERTY = "valladolid"
 
 // variables
 let width = 0
@@ -26,17 +28,12 @@ const percent = (num = 0) => num.toLocaleString(undefined, { style: "percent" })
 const formatDate = (date = new Date(), opts = {}) => date.toLocaleDateString(undefined, opts)
 const parser = ({ name, a, b, date: d }) => ({ name, result: +b !== 0 ? 1 - (+a/+b) : 0, date: new Date(d) })
 
-// projections
-const projection = geoMercator()
-const path = geoPath(projection)
-const feat = feature(data, data.objects.valladolid)
-
 // static elements
 const map = styler(select("#map"), { width: "100%", height: "100%" })
 const svg = map.append("svg")
-const gi = svg.append("g") //mitems
-const gl = svg.append("g") //legend
-const gm = svg.append("g") //month
+const gpath = svg.append("g")
+const glegend = svg.append("g")
+const gmonth = svg.append("g")
 const tooltip = styler(map.append("div"), {
   opacity: 0,
   position: "absolute",
@@ -48,38 +45,37 @@ const tooltip = styler(map.append("div"), {
   "transition": "opacity 250ms"
 })
 
-// render functions
-const size = (fn = x => x) => {
+// map functions
+const size = (fn = x => x, params = {}) => {
   ({ width: width, height: height } = map.node().getBoundingClientRect())
   svg.attr("viewBox", [0, 0, width, height])
-  projection.fitSize([width,height], feat);
+  
+  const { geojson } = params
+  projection.fitSize([width,height], geojson)
 
-  return fn()
+  return fn(params)
 }
 
-const render = (data = [], { year: currentYear = 0, month: currentMonth = 0 } = {}) => {
-  const { features } = feat
+const render = ({ geojson = [], csv = [], year: currentYear = 0, month: currentMonth = 0 }) => {
   // merge geodata with csvdata
-  const edited = features.map(f => {
+  const features = geojson.features.map(f => {
     const { properties: { nombre }} = f
-    const match = data.find(({ name, date }) => name === nombre && date.getMonth() === currentMonth && date.getFullYear() === currentYear)
+    const match = csv.find(({ name, date }) => name === nombre && date.getMonth() === currentMonth && date.getFullYear() === currentYear)
 
     return { ...f, properties: { ...f.properties, ...match }}
   })
 
-  feat.features = edited
-
   const range = schemeGreens[5]
-  const color = scaleQuantile(range).domain(extent(data, ({ result }) => result))
+  const color = scaleQuantile(range).domain(extent(csv, ({ result }) => result))
   
   // draw elements
-  const items = gi.selectAll("path").data(feat.features)
-  const legend = gl.selectAll("rect").data(range)
-  const month = gm.selectAll("text").data([{ currentYear, currentMonth }])
+  const paths = gpath.selectAll("path").data(features)
+  const legend = glegend.selectAll("rect").data(range)
+  const month = gmonth.selectAll("text").data([{ currentYear, currentMonth }])
   const monthEnter = month.enter().append("text")
   const rectEnter = legend.enter().append("rect")
   const textEnter = legend.enter().append("text")
-  const pathEnter = items.enter().append("path")
+  const pathEnter = paths.enter().append("path")
   
   legend
     .merge(rectEnter)
@@ -123,41 +119,43 @@ const render = (data = [], { year: currentYear = 0, month: currentMonth = 0 } = 
       return formatDate(date, { year: "2-digit", month: "short" })
     })
 
-  items
+  paths
     .on("mouseenter", ({ pageX, pageY }, { properties: {name, date, result} }) => {
       styler(tooltip, { opacity: 1, top: `${pageY}px`, left: `${pageX}px` })
       tooltip.html(`${name}: <em>${percent(result)}</em>`)
     })
-    .on("mouseleave", (e) => styler(tooltip, { opacity: 0 }))
+    .on("mouseleave", e => styler(tooltip, { opacity: 0 }))
     .merge(pathEnter)
     .transition()
-    .attr("d", d => path(d))
+    .attr("d", d => geoPath(projection)(d))
     .attr("fill", ({ properties: { result } = {}} = {}) => color(result))
     .attr("stroke","white")
 
-  items
+  paths
     .exit()
     .remove()
 }
 
 // request data
-dsv(DELIMITER, REPORT_PATH, parser).then(r => {
-  const months = [... new Set(r.map(({ date }) => JSON.stringify({ year: date.getFullYear(), month: date.getMonth() })))]
-    .map(JSON.parse)
-    .sort((a, b) => {
-      if (a.year > b.year) return 1
-      if (a.year < b.year) return -1
-      if (a.month > b.month) return 1
-      if (a.month < b.month) return -1
-    })
+Promise.all([fetch(TOPOJSON_PATH).then(r => r.json()), dsv(DELIMITER, REPORT_PATH, parser)])
+  .then(([topojson, csv]) => {
+    const geojson = feature(topojson, topojson.objects[TOPOJSON_PROPERTY])
+    const months = [... new Set(csv.map(({ date }) => JSON.stringify({ year: date.getFullYear(), month: date.getMonth() })))]
+      .map(JSON.parse)
+      .sort((a, b) => {
+        if (a.year > b.year) return 1
+        if (a.year < b.year) return -1
+        if (a.month > b.month) return 1
+        if (a.month < b.month) return -1
+      })
 
-  let i = 0
-  // initial call with sizes
-  size(() => render(r, months[i]))
-  const interval = setInterval(() => {
-    render(r, months[i++])
-    if (i >= months.length) clearInterval(interval)
-  }, INTERVAL_TIME)
-  
-  window.addEventListener("resize", () => size(() => render(r, months[i])))
-})
+    let i = 0
+    // initial call to get sizes
+    size(render, { geojson, csv, ...months[i] })
+    const interval = setInterval(() => {
+      render({ geojson, csv, ...months[i++] })
+      if (i >= months.length) clearInterval(interval)
+    }, INTERVAL_TIME)
+    
+    window.addEventListener("resize", () => size(render, { geojson, csv, ...months[i] }))
+  })
