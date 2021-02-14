a02509e0e44202b462ab7efb09b084459e37d8ce
Hugoren Martinako <aumpfbahn@gmail.com>
2021-02-14 20:09:36 +0100

    add another places

---
+++ src/js/config.js
@@ -0,0 +1,87 @@
+import AN from "url:../static/AN.topo.json"
+import AS from "url:../static/AS.topo.json"
+import AR from "url:../static/AR.topo.json"
+import CN from "url:../static/CN.topo.json"
+import CB from "url:../static/CB.topo.json"
+import CL from "url:../static/CL.topo.json"
+import CM from "url:../static/CM.topo.json"
+import CT from "url:../static/CT.topo.json"
+import M from "url:../static/M.topo.json"
+import VC from "url:../static/VC.topo.json"
+import PV from "url:../static/PV.topo.json"
+import EX from "url:../static/EX.topo.json"
+import GA from "url:../static/GA.topo.json"
+import IB from "url:../static/IB.topo.json"
+import RI from "url:../static/RI.topo.json"
+import NA from "url:../static/NA.topo.json"
+import MU from "url:../static/MU.topo.json"
+import ES from "url:../static/ES.topo.json"
+
+export const FEATURE_ID = "id"
+export const FEATURE_VALUES = "values"
+export const FEATURE_DESC = "name"
+export const URLS = [
+  { url: AN, code: "ES-AN", prop: "AN" },
+  { url: AS, code: "ES-AS", prop: "AS" },
+  { url: AR, code: "ES-AR", prop: "AR" },
+  { url: CN, code: "ES-CN", prop: "CN" },
+  { url: CB, code: "ES-CB", prop: "CB" },
+  { url: CL, code: "ES-CL", prop: "CL" },
+  { url: CM, code: "ES-CM", prop: "CM" },
+  { url: CT, code: "ES-CT", prop: "CT" },
+  { url: M, code: "ES-M", prop: "M" },
+  { url: VC, code: "ES-VC", prop: "VC" },
+  { url: PV, code: "ES-PV", prop: "PV" },
+  { url: EX, code: "ES-EX", prop: "EX" },
+  { url: GA, code: "ES-GA", prop: "GA" },
+  { url: IB, code: "ES-IB", prop: "IB" },
+  { url: RI, code: "ES-RI", prop: "RI" },
+  { url: NA, code: "ES-NA", prop: "NA" },
+  { url: MU, code: "ES-MU", prop: "MU" }
+]
+export const ELEMENTS = [
+  {
+    value: "Castilla y León",
+    code: null,
+  },
+  {
+    value: "Ávila",
+    code: "05",
+  },
+  {
+    value: "Burgos",
+    code: "09",
+  },
+  {
+    value: "León",
+    code: "24",
+  },
+  {
+    value: "Asturias",
+    code: "33",
+  },
+  {
+    value: "Palencia",
+    code: "34",
+  },
+  {
+    value: "Salamanca",
+    code: "37",
+  },
+  {
+    value: "Segovia",
+    code: "40",
+  },
+  {
+    value: "Soria",
+    code: "42",
+  },
+  {
+    value: "Valladolid",
+    code: "47",
+  },
+  {
+    value: "Zamora",
+    code: "49",
+  },
+]
--- src/js/index.js
+++ src/js/index.js
@@ -8,12 +8,12 @@ import { schemeGreens } from "d3-scale-chromatic"
 import { zoom, zoomIdentity } from "d3-zoom"
 import { feature } from "topojson-client"
 import { percent, formatDate, get } from "./helpers"
-import { ELEMENTS, TOPOJSON_PROP, TOPOJSON_URL, FEATURE_ID, FEATURE_VALUES, FEATURE_DESC } from "./elements"
+import { ELEMENTS, URLS, FEATURE_ID, FEATURE_VALUES, FEATURE_DESC } from "./config"
 
 // constants
 const INTERVAL_TIME = 1000
 const projection = geoMercator()
-const range = schemeGreens[9]
+const range = schemeGreens[5]
 const colorScale = scaleQuantile(range).domain([0, 1])
 const timeScale = scalePoint()
 const z = zoom().scaleExtent([1, 8])
@@ -119,7 +119,24 @@ sidebar
     ({ code }) => code === ELEMENTS[currentFeatureIx].code || null
   )
   .text(x => x.value)
-  
+
+// TODO: testing
+if (false) {
+  sidebar
+    .append("div")
+    .attr("class", "selector card")
+    .append("select")
+    .on("change", ({ target: { value } }) => {
+      const { url, prop } = URLS.find(x => x.code === value)
+      reload(fetch(url).then(r => r.json()), prop)
+    })
+    .selectAll("option")
+    .data(URLS)
+    .join("option")
+    .attr("value", ({ code }) => code)
+    .text(x => x.code)
+}
+
 const lineSlider = slider
   .append("line")
   .attr("x1", 0)
@@ -183,7 +200,7 @@ const render = () => {
   const isFeatureActive = d => {
     const { code } = ELEMENTS[currentFeatureIx]
     if (!code) return true
-    return code === getId(d).substring(0, 2)
+    return code === getId(d)?.substring(0, 2)
   }
 
   const [[x0, y0], [x1, y1]] = geoPath(projection).bounds({
@@ -223,7 +240,7 @@ const render = () => {
             .raise()
             .attr("stroke-width", 2)
             .transition(t)
-            .attr("fill", "#7d0633")
+            .attr("fill", "#ffcaba")
             .attr("stroke", "#310234")
             
           tooltip
@@ -267,21 +284,23 @@ const resize = () => {
   ({ width: w, height: h } = map.node().getBoundingClientRect())
   svg.attr("viewBox", [0, 0, w, h])
   projection.fitSize([w, h], geojson)
-  
+
   const [from, to] = [w < 640 ? w * 0.05 : w * 0.6, w * 0.95]
   timeScale.range([from, to])
   lineSlider.attr("x2", to - from)
   ghostLineSlider.attr("x2", to - from)
   slider.attr("transform", `translate(${from} ${w < 640 ? h * 0.85 : h * 0.95})`).style("opacity", 1)
-    
+
   render()
 }
 
-const reload = async (...promises) => {
-  const [topojson] = await Promise.all(promises)
+const reload = async (promise, prop) => {
+  loader.style("opacity", 1)
+  
+  const topojson = await promise
   geojson = feature(
     topojson,
-    topojson.objects[TOPOJSON_PROP]
+    topojson.objects[prop]
   )
 
   // set the differents month-year tuples
@@ -303,6 +322,7 @@ const reload = async (...promises) => {
 }
 
 // init app
-reload(fetch(TOPOJSON_URL).then((r) => r.json()))
+const { url, prop } = URLS[5] // CYL
+reload(fetch(url).then(r => r.json()), prop)
 
 addEventListener("resize", resize)
