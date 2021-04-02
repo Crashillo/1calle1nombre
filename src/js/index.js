f17a9bd531ea5b7c1d2f9da626c4e457a7b78b8f
Hugoren Martinako <aumpfbahn@gmail.com>
2021-04-02 15:32:46 +0200

    naming

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,16 @@
 /* global process */
-import Visor from "./visor"
+import Viewer from "./viewer"
 import { URLS } from "./config"
 
 if (process.env.BUILD) {
   console.info(`Last build: ${new Date(process.env.BUILD * 1e3).toISOString()}`)
 }
 
-const visor = new Visor()
+const viewer = new Viewer()
 
 // init app
 const [[{ url: ccaa }, { url: mun }]] = URLS // ES
 Promise.all([
   fetch(ccaa).then(r => r.json()),
   fetch(mun).then(r => r.json()),
-]).then(([features, lines]) => visor.load({ features, lines }))
+]).then(([features, lines]) => viewer.load({ features, lines }))
