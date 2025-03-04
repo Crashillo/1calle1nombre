870e949b19dd28be4d93614e192f3f83f43eb564
Hugoren Martinako <aumpfbahn@gmail.com>
2025-03-04 18:10:12 +0100

    fetch new topojsons

--- src/js/config.js
+++ src/js/config.js
@@ -1,24 +1,76 @@
+import A from "url:../../static/A.topo.json"
+import AB from "url:../../static/AB.topo.json"
+import AL from "url:../../static/AL.topo.json"
+import AV from "url:../../static/AV.topo.json"
+import B from "url:../../static/B.topo.json"
+import BA from "url:../../static/BA.topo.json"
+import BI from "url:../../static/BI.topo.json"
+import BU from "url:../../static/BU.topo.json"
+import C from "url:../../static/C.topo.json"
+import CA from "url:../../static/CA.topo.json"
+import CC from "url:../../static/CC.topo.json"
+import CE from "url:../../static/CE.topo.json"
+import CO from "url:../../static/CO.topo.json"
+import CR from "url:../../static/CR.topo.json"
+import CS from "url:../../static/CS.topo.json"
+import CU from "url:../../static/CU.topo.json"
+import GC from "url:../../static/GC.topo.json"
+import GI from "url:../../static/GI.topo.json"
+import GR from "url:../../static/GR.topo.json"
+import GU from "url:../../static/GU.topo.json"
+import H from "url:../../static/H.topo.json"
+import HU from "url:../../static/HU.topo.json"
+import J from "url:../../static/J.topo.json"
+import L from "url:../../static/L.topo.json"
+import LE from "url:../../static/LE.topo.json"
+import LO from "url:../../static/LO.topo.json"
+import LU from "url:../../static/LU.topo.json"
+import M from "url:../../static/M.topo.json"
+import MA from "url:../../static/MA.topo.json"
+import ML from "url:../../static/ML.topo.json"
+import MU from "url:../../static/MU.topo.json"
+import NA from "url:../../static/NA.topo.json"
+import O from "url:../../static/O.topo.json"
+import OR from "url:../../static/OR.topo.json"
+import P from "url:../../static/P.topo.json"
+import PM from "url:../../static/PM.topo.json"
+import PO from "url:../../static/PO.topo.json"
+import S from "url:../../static/S.topo.json"
+import SA from "url:../../static/SA.topo.json"
+import SE from "url:../../static/SE.topo.json"
+import SG from "url:../../static/SG.topo.json"
+import SO from "url:../../static/SO.topo.json"
+import SS from "url:../../static/SS.topo.json"
+import T from "url:../../static/T.topo.json"
+import TE from "url:../../static/TE.topo.json"
+import TF from "url:../../static/TF.topo.json"
+import TO from "url:../../static/TO.topo.json"
+import V from "url:../../static/V.topo.json"
+import VA from "url:../../static/VA.topo.json"
+import VI from "url:../../static/VI.topo.json"
+import Z from "url:../../static/Z.topo.json"
+import ZA from "url:../../static/ZA.topo.json"
 import AN from "url:../../static/AN.topo.json"
-import AS from "url:../../static/AS.topo.json"
+// import AS from "url:../../static/AS.topo.json"
 import AR from "url:../../static/AR.topo.json"
 import CN from "url:../../static/CN.topo.json"
-import CB from "url:../../static/CB.topo.json"
+// import CB from "url:../../static/CB.topo.json"
 import CL from "url:../../static/CL.topo.json"
 import CM from "url:../../static/CM.topo.json"
 import CT from "url:../../static/CT.topo.json"
-import MD from "url:../../static/MD.topo.json"
+// import MD from "url:../../static/MD.topo.json"
 import VC from "url:../../static/VC.topo.json"
 import PV from "url:../../static/PV.topo.json"
 import EX from "url:../../static/EX.topo.json"
 import GA from "url:../../static/GA.topo.json"
-import IB from "url:../../static/IB.topo.json"
-import RI from "url:../../static/RI.topo.json"
-import NC from "url:../../static/NC.topo.json"
-import MC from "url:../../static/MC.topo.json"
-import CE from "url:../../static/CE.topo.json"
-import ML from "url:../../static/ML.topo.json"
-import ccaa from "url:../../static/ES.ccaa.topo.json"
-import prov from "url:../../static/ES.prov.topo.json"
+// import IB from "url:../../static/IB.topo.json"
+// import RI from "url:../../static/RI.topo.json"
+// import NC from "url:../../static/NC.topo.json"
+// import MC from "url:../../static/MC.topo.json"
+// import CE from "url:../../static/CE.topo.json"
+// import ML from "url:../../static/ML.topo.json"
+
+import ES from "url:../../static/ES.ccaa.topo.json"
 
 export const FEATURE_ID = "id"
 export const FEATURE_VALUES = "values"
@@ -26,26 +78,76 @@ export const FEATURE_DESC = "name"
 export const FEATURE_CODE = "prov"
 
 export const URLS = [
-  [{ url: ccaa }, { url: prov }],
-  [
-    { url: AN, code: "ES-AN" },
-    { url: AS, code: "ES-AS" },
-    { url: AR, code: "ES-AR" },
-    { url: CN, code: "ES-CN" },
-    { url: CB, code: "ES-CB" },
-    { url: CL, code: "ES-CL" },
-    { url: CM, code: "ES-CM" },
-    { url: CT, code: "ES-CT" },
-    { url: MD, code: "ES-MD" },
-    { url: VC, code: "ES-VC" },
-    { url: PV, code: "ES-PV" },
-    { url: EX, code: "ES-EX" },
-    { url: GA, code: "ES-GA" },
-    { url: IB, code: "ES-IB" },
-    { url: RI, code: "ES-RI" },
-    { url: NC, code: "ES-NC" },
-    { url: MC, code: "ES-MC" },
-    { url: CE, code: "ES-CE" },
-    { url: ML, code: "ES-ML" }
-  ]
+  { url: ES },
+  //
+  { url: AN, code: "ES-AN" },
+  { url: O, code: "ES-AS" },
+  { url: AR, code: "ES-AR" },
+  { url: CN, code: "ES-CN" },
+  { url: S, code: "ES-CB" },
+  { url: CL, code: "ES-CL" },
+  { url: CM, code: "ES-CM" },
+  { url: CT, code: "ES-CT" },
+  { url: M, code: "ES-MD" },
+  { url: VC, code: "ES-VC" },
+  { url: PV, code: "ES-PV" },
+  { url: EX, code: "ES-EX" },
+  { url: GA, code: "ES-GA" },
+  { url: PM, code: "ES-IB" },
+  { url: LO, code: "ES-RI" },
+  { url: NA, code: "ES-NC" },
+  { url: MU, code: "ES-MC" },
+  { url: CE, code: "ES-CE" },
+  { url: ML, code: "ES-ML" },
+  //
+  { url: A, code: "ES-A" },
+  { url: AB, code: "ES-AB" },
+  { url: AL, code: "ES-AL" },
+  { url: AV, code: "ES-AV" },
+  { url: B, code: "ES-B" },
+  { url: BA, code: "ES-BA" },
+  { url: BI, code: "ES-BI" },
+  { url: BU, code: "ES-BU" },
+  { url: C, code: "ES-C" },
+  { url: CA, code: "ES-CA" },
+  { url: CC, code: "ES-CC" },
+  { url: CO, code: "ES-CO" },
+  { url: CR, code: "ES-CR" },
+  { url: CS, code: "ES-CS" },
+  { url: CU, code: "ES-CU" },
+  { url: GC, code: "ES-GC" },
+  { url: GI, code: "ES-GI" },
+  { url: GR, code: "ES-GR" },
+  { url: GU, code: "ES-GU" },
+  { url: H, code: "ES-H" },
+  { url: HU, code: "ES-HU" },
+  { url: J, code: "ES-J" },
+  { url: L, code: "ES-L" },
+  { url: LE, code: "ES-LE" },
+  // { url: LO, code: "ES-LO" },
+  { url: LU, code: "ES-LU" },
+  // { url: M, code: "ES-M" },
+  { url: MA, code: "ES-MA" },
+  // { url: MU, code: "ES-MU" },
+  // { url: NA, code: "ES-NA" },
+  // { url: O, code: "ES-O" },
+  { url: OR, code: "ES-OR" },
+  { url: P, code: "ES-P" },
+  // { url: PM, code: "ES-PM" },
+  { url: PO, code: "ES-PO" },
+  // { url: S, code: "ES-S" },
+  { url: SA, code: "ES-SA" },
+  { url: SE, code: "ES-SE" },
+  { url: SG, code: "ES-SG" },
+  { url: SO, code: "ES-SO" },
+  { url: SS, code: "ES-SS" },
+  { url: T, code: "ES-T" },
+  { url: TE, code: "ES-TE" },
+  { url: TF, code: "ES-TF" },
+  { url: TO, code: "ES-TO" },
+  { url: V, code: "ES-V" },
+  { url: VA, code: "ES-VA" },
+  { url: VI, code: "ES-VI" },
+  { url: Z, code: "ES-Z" },
+  { url: ZA, code: "ES-ZA" }
 ]
